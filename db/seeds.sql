6e9015c4e2f44865e0c19ae141e530ba38111156
Claudia <claudiacchoi@gmail.com>
2019-04-19 17:25:57 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES ("Acadia"), ("Au Cheval"), ("The Bad Apple"), ("Billy Goat Tavern"), ("bopNgrill");
+
+SELECT * FROM burgers;
+© 2019 GitHub, Inc.
